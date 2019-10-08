76632498187829424f43099958a8603b4d1db22d
Sarvesh Dubey <38752758+dubesar@users.noreply.github.com>
2019-10-08 19:33:32 +0530

    Create JavaStdinandStdoutI.java

---
+++ JavaStdinandStdoutI.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+
