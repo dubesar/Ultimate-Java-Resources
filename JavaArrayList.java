05c289344958c4082e6d549dcbf570965a7122e2
aman <rsudhanshu467@gmail.com>
2019-10-08 20:05:10 +0530

    added ArrayList

---
+++ JavaArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+ class ArrayListsolution{  
+ public static void main(String args[]){  
+ArrayList<String> list=new ArrayList<String>();//Creating arraylist    
+      list.add("Ravi");//Adding object in arraylist    
+      list.add("Vijay");    
+      list.add("Ravi");    
+      list.add("Ajay");    
+      //Invoking arraylist object   
+      System.out.println(list);  
+  }  
+ }  
+}  
