aee53c02ca031a8def80a757d09e24cd88357637
MisaelSantos <44177887+MisaelSantos@users.noreply.github.com>
2019-10-08 12:13:23 -0300

    Update JavaStdinandStdoutI.java
    Comments

--- JavaStdinandStdoutI.java
+++ JavaStdinandStdoutI.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+        Scanner scan = new Scanner(System.in); 
+        //reading of integers
+        int a = scan.nextInt(); 
         int b = scan.nextInt();
         int c = scan.nextInt();
-        // Complete this line
-
+        
+        //priting of integers
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
     }
 }
-
