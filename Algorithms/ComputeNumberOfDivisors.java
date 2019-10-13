35cdfa1962a939f2445e15f0c729e5e5e17af774
rajnis09 <rajnish.kumar2998@gmail.com>
2019-10-13 21:31:29 +0530

    Added fastest way to compute number of divisors of a number

--- Algorithms/ComputeNumberOfDivisors.java
+++ Algorithms/ComputeNumberOfDivisors.java
@@ -1,23 +1,65 @@
 class ComputeNumberOfDivisors {
 
-    // Function that calculates number of divisors
-    // Time Complexity : O( (n)^ 1/2 )
-    static int numberOfDivisors(int n) {
-        int count = 0;
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                if (n / i == i) {
-                    count++;
-                } else {
-                    count += 2;
-                }
+    // Method to find prime numbers
+    static void SieveOfEratosthenes(int n, boolean prime[]) {
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true) {
+                for (int i = p * 2; i <= n; i += p)
+                    prime[i] = false;
             }
         }
-        return count;
+    }
+
+    // Method to count divisors
+    static int countDivisors(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        boolean prime[] = new boolean[n + 1];
+        boolean primeSquare[] = new boolean[(n * n) + 1];
+        int a[] = new int[n];
+
+        prime[1] = false;
+        for (int i = 2; i <= n; i++) {
+            prime[i] = true;
+        }
+
+        SieveOfEratosthenes(n, prime);
+        int j = 0;
+        for (int p = 2; p <= n; p++) {
+            if (prime[p]) {
+                a[j] = p;
+                primeSquare[p * p] = true;
+                j++;
+            }
+        }
+        int ans = 1;
+        for (int i = 0;; i++) {
+            if (n < a[i] * a[i] * a[i]) {
+                break;
+            }
+            int cnt = 1;
+            while (n % a[i] == 0) {
+                n = n / a[i];
+                cnt = cnt + 1;
+            }
+            ans = ans * cnt;
+        }
+        if (prime[n]) {
+            ans = ans * 2;
+        } else if (primeSquare[n]) {
+            ans = ans * 3;
+        } else if (n != 1) {
+            ans = ans * 4;
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
-        int x = 25;
-        System.out.println("Total number of divisor of " + x + " are " + numberOfDivisors(x));
+        int x = 100;
+        System.out.println("Total number of divisors of " + x + " are " + countDivisors(x));
     }
 }
+
+// Time Complexity : O( n ^ (1/3) )
