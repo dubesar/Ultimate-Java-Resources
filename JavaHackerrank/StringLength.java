c3db419e45425aea04a1f83e4d2e2d67cce68510
kartik-manchanda <kartikmanchanda1901@gmail.com>
2020-10-01 15:56:36 +0530

    added StringLenth Java Program

---
+++ JavaHackerrank/StringLength.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+
+    // create a string
+    String greet = "Hello! World";
+    System.out.println("The string is: " + greet);
+
+    //checks the string length
+    System.out.println("The length of the string: " + greet.length());
+  }
+}
