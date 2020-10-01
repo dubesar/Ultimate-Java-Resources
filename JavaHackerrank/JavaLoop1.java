9849816b74482d18b290bdc634b1b22e5ad3e44c
lakshay rawat <65499159+Lakshayrawat@users.noreply.github.com>
2020-10-01 15:37:37 +0530

    Create JavaLoop1.java

---
+++ JavaHackerrank/JavaLoop1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i=1;i<=10;i++){
+            System.out.println(N+" " +"x"+" "+i+" "+"="+" "+N*i );
+        }
+        scanner.close();
+    }
+}
