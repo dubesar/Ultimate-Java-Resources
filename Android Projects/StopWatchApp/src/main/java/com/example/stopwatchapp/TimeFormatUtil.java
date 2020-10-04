ddf6198e2fb2153bf2bbe36d9ccb20583e101be2
Shikhar Srivastava <60563576+shikhar8434@users.noreply.github.com>
2020-10-04 20:31:53 +0530

    StopWatchApp added
    Stopwatch app added with stopwatch background sound

---
+++ Android Projects/StopWatchApp/src/main/java/com/example/stopwatchapp/TimeFormatUtil.java
@@ -0,0 +1,36 @@
+package com.example.stopwatchapp;
+
+
+import android.util.Log;
+
+public class TimeFormatUtil {
+
+    public static String toDisplayString(int timeHundreds){
+        int hundreds,seconds,minutes;
+
+        String[] formatterArrayMillis = new String[2];
+        String formattedSeconds,formattedMinutes;
+
+        hundreds=timeHundreds%100;
+        String milliSecStr = Integer.toString(hundreds);
+        formatterArrayMillis[0] = "0" + milliSecStr;
+        formatterArrayMillis[1] = milliSecStr;
+
+        seconds=(timeHundreds/=100)%60;
+        minutes=(timeHundreds/=60)%60;
+
+        formattedSeconds=Integer.toString(seconds/10)+
+                Integer.toString(seconds%10);
+        formattedMinutes=Integer.toString(minutes/10)+
+                Integer.toString(minutes%10);
+
+        int millSecDigitsCnt = milliSecStr.length();
+        String timeString =
+                formattedMinutes+":"+
+                        formattedSeconds+"."+
+                        formatterArrayMillis[millSecDigitsCnt - 1];
+
+        return timeString;
+    }
+
+}
