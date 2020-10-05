e3efa305f1043aac2e4c8f69de6dd535f9768ebc
harsh tiwari <harsh.tiwari261@gmail.com>
2020-10-05 12:52:04 +0530

    binarytodecimal.c

---
+++ Math/binarytodecimal.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <stdio.h>
+int convert(long long n);
+int main() {
+    long long n;
+    printf("Enter a binary number: ");
+    scanf("%lld", &n);
+    printf("%lld in binary = %d in decimal", n, convert(n));
+    return 0;
+}
+
+int convert(long long n) {
+    int dec = 0, i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+    return dec;
+}
