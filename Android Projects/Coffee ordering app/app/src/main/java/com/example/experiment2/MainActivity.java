3de59357ed8eb7fb972a4ae3ea6a9cc33b78d510
Samriddhi Arya <samriddhiarya@gmail.com>
2020-10-05 17:24:12 +0530

    Added an android project

---
+++ Android Projects/Coffee ordering app/app/src/main/java/com/example/experiment2/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.experiment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView q = (TextView)findViewById(R.id.quantity);
+
+        Button b1 = (Button)findViewById(R.id.decrease);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(quantity > 1){
+                    quantity -= 1;
+                }
+                if(quantity == 1){
+                    Context context = getApplicationContext();
+                    CharSequence text = "You cannot order less than 1 coffee!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                }
+                q.setText("" + quantity);
+            }
+        });
+
+        Button b2 = (Button)findViewById(R.id.increase);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(quantity < 10){
+                    quantity += 1;
+                }
+                q.setText("" + quantity);
+            }
+        });
+
+        final EditText e1 = (EditText)findViewById(R.id.name);
+        final EditText e2 = (EditText)findViewById(R.id.address);
+
+        Button b3 = (Button)findViewById(R.id.submit);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Order placed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b3.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                String name = "Name: " + e1.getText().toString();
+                String address = "Address: " + e2.getText().toString();
+                String quant = "Quantity: " + q.getText().toString();
+
+
+                Intent i = new Intent(MainActivity.this,SecondActivity.class);
+
+                //For Passing the Values to Second Activity
+                i.putExtra("name_key", name);
+                i.putExtra("reg_key",address);
+                i.putExtra("dept_key", quant);
+
+                startActivity(i);
+                return false;
+            }
+
+        });
+    }
+}
---
+++ Android Projects/Coffee ordering app/app/src/main/java/com/example/experiment2/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.experiment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView t1 = (TextView) findViewById(R.id.textView1);
+        TextView t2 = (TextView) findViewById(R.id.textView2);
+        TextView t3 = (TextView) findViewById(R.id.textView3);
+        TextView t4 = (TextView) findViewById(R.id.textView4);
+        TextView t5 = (TextView) findViewById(R.id.textView5);
+
+        //Getting the Intent
+        Intent i = getIntent();
+
+        //Getting the Values from First Activity using the Intent received
+        String name = i.getStringExtra("name_key");
+        String address = i.getStringExtra("reg_key");
+        String quantity = i.getStringExtra("dept_key");
+
+        //Setting the Values to Intent
+        t1.setText(name);
+        t2.setText(address);
+        t3.setText(quantity);
+    }
+}
