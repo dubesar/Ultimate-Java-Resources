5215c4ce628656f7beaf9016fa02a6033983cf4f
vishwajeet7200 <43901984+vishwajeet7200@users.noreply.github.com>
2020-10-06 20:30:58 +0530

    Update HashMapExample.java
    new example with proper understanding

--- Data Structures/HashMapExample.java
+++ Data Structures/HashMapExample.java
@@ -1,43 +1,33 @@
 
-// Import the HashMap class
-import java.util.HashMap;
-
-public class HashMapExample {
-    public static void main(String[] args) {
-        // Create a HashMap object called map
-        HashMap<String, String> map = new HashMap<String, String>();
-
-        // Add keys and values (Country, City)
-        map.put("England", "London");
-        map.put("Germany", "Berlin");
-        map.put("Norway", "Oslo");
-        map.put("USA", "Washington DC");
-
-        // Prints key value pairs
-        System.out.println(map);
-
-        // To access value
-        map.get("England");
-
-        // To remove an item
-        map.remove("England");
-
-        // To find out the number of items
-        map.size();
-
-        // Looping through the items in HashMap
-        // and printing the keys
-        for (String i : map.keySet()) {
-            System.out.println(i);
-        }
-
-        // Looping through the items in HashMap
-        // and printing the values
-        for (String i : map.values()) {
-            System.out.println(i);
-        }
-
-        // To remove all items
-        map.clear();
-    }
-}
+// Java program to illustrate 
+// Java.util.HashMap 
+  
+import java.util.HashMap; 
+  
+public class GFG { 
+    public static void main(String[] args) 
+    { 
+        // Create an empty hash map 
+        HashMap<String, Integer> map 
+            = new HashMap<>(); 
+  
+        // Add elements to the map 
+        map.put("vishal", 10); 
+        map.put("sachin", 30); 
+        map.put("vaibhav", 20); 
+  
+        // Print size and content 
+        System.out.println("Size of map is:- "
+                           + map.size()); 
+        System.out.println(map); 
+  
+        // Check if a key is present and if 
+        // present, print value 
+        if (map.containsKey("vishal")) { 
+            Integer a = map.get("vishal"); 
+            System.out.println("value for key"
+                               + " \"vishal\" is:- "
+                               + a); 
+        } 
+    } 
+} 
