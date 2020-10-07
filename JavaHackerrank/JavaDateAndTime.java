d449b466fcf9298897faf6b714f60b563ea6b344
mete23 <67523122+mete23@users.noreply.github.com>
2020-10-07 19:42:45 +0200

    Update JavaDateAndTime.java
    This solution is much shoorter

--- JavaHackerrank/JavaDateAndTime.java
+++ JavaHackerrank/JavaDateAndTime.java
@@ -1,29 +1,9 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import java.util.Scanner;
 import java.time.LocalDate;
 
-class Result {
-
-    /*
-     * Complete the 'findDay' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER month
-     *  2. INTEGER day
-     *  3. INTEGER year
-     */
-
-    public static String findDay(int month, int day, int year) {
+public class Solution {
+    public static String getDay(String day, String month, String year) {
         int d = Integer.valueOf(day);
         int m = Integer.valueOf(month);
         int y = Integer.valueOf(year);
@@ -31,27 +11,12 @@ class Result {
         return date.getDayOfWeek().toString();
     }
 
-}
-
-public class JavaDateAndTime {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int month = Integer.parseInt(firstMultipleInput[0]);
-
-        int day = Integer.parseInt(firstMultipleInput[1]);
-
-        int year = Integer.parseInt(firstMultipleInput[2]);
-
-        String res = Result.findDay(month, day, year);
-
-        bufferedWriter.write(res);
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day   = in.next();
+        String year  = in.next();
+        
+        System.out.println(getDay(day, month, year));
     }
 }
