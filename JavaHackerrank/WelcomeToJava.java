e8a8114bb629c2360f4d5059e8cc2f63bb13d4e2
Naziya Parveen <naziyap018@gmail.com>
2020-10-07 17:21:21 +0530

    Welcome to Java program added

---
+++ JavaHackerrank/WelcomeToJava.java
@@ -0,0 +1,9 @@
+//Getting Started with Java
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello, World.\nHello, Java.");
+    }
+}
