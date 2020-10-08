9ea2ab75a0fcd1bd7ce37eafe21ea23a26fd6162
DJ Nautiyal <dhannanjainautiyal@gmail.com>
2020-10-08 22:19:01 +0530

    Made document more readable

--- Design Patterns/BuilderPattern.java
+++ Design Patterns/BuilderPattern.java
@@ -2,12 +2,11 @@
     Builder pattern is a creational design pattern that lets you construct complex objects step by step.
     The pattern allows you to produce different types and representations of an object
     using the same construction code
-    Objective: To get rid of complex Constructors
+    Objective: To get rid of complex Constructors and making object creation easier to read.
 
     link: https://refactoring.guru/design-patterns/builder
  */
-public class AccountBuilder {
-
+class AccountBuilder {
     private long accountNumber;
     private String owner;
     private double balance;
@@ -52,42 +51,10 @@ public class AccountBuilder {
         }
     }
 
-    //Important: Make constructor private - to make builder class very important
+    //Important: Make constructor private - to make builder class only way possible
     private AccountBuilder(){}
 
-    // needed boiler-plate
-
-    public long getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(long accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public double getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(double interestRate) {
-        this.interestRate = interestRate;
-    }
+    // getters and setters - avoiding for readability purposes
 
     @Override
     public String toString() {
@@ -98,14 +65,16 @@ public class AccountBuilder {
                 ", interestRate=" + interestRate +
                 '}';
     }
+}
 
-    // try this now;
+public class BuilderPattern{
+    // try this now; The instantiation of an object looks easier to read
     public static void main(String[] args) {
         AccountBuilder firstAccount = new AccountBuilder.Builder(548263L)
                 .hasBalance(1000L)
                 .hasOwner("John")
                 .hasInterestRate(1.2)
-                .build(); // now it's more verbose and more human readable
+                .build();
 
         AccountBuilder secondAccount = new AccountBuilder.Builder(2658745L)
                 .hasOwner("Jane")
