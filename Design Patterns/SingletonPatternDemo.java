a84dfbac0fa70206bd4496088f5f2b6fab0a5e3f
Percival Tapera <perctapera@gmail.com>
2020-10-09 15:57:52 +0200

    added a singleton design pattern example

---
+++ Design Patterns/SingletonPatternDemo.java
@@ -0,0 +1,14 @@
+public class SingletonPatternDemo {
+   public static void main(String[] args) {
+
+      //illegal construct
+      //Compile Time Error: The constructor SingleObject() is not visible
+      //SingleObject object = new SingleObject();
+
+      //Get the only object available
+      SingleObject object = SingleObject.getInstance();
+
+      //show the message
+      object.showMessage();
+   }
+}
