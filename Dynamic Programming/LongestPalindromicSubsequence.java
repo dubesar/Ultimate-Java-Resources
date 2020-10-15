3867f0222b6d770f2577d6edac5828e2b3d38792
Jayit Saha <45602643+ricksaha2000@users.noreply.github.com>
2020-10-15 19:52:57 +0530

    Create LongestPalindromicSubsequence.java

---
+++ Dynamic Programming/LongestPalindromicSubsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        // states
+        int[][] f = new int[n][n];
+        // function
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (len == 1) {
+                    f[i][j] = 1;  // initialization
+                } else if (s.charAt(i) == s.charAt(j)) {
+                    f[i][j] = f[i + 1][j - 1] + 2;
+                } else {
+                    f[i][j] = Math.max(f[i + 1][j], f[i][j - 1]);
+                }
+            }
+        }
+        // answer
+        return f[0][n - 1];
+    }
+}
