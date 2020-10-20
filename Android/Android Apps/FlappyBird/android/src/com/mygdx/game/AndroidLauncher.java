326cd9448ffed37b4b34dab0fe98037df248c037
AntoineDoniel <panka9988@gmail.com>
2020-10-20 16:40:52 +0530

    added flappy bird

---
+++ Android/Android Apps/FlappyBird/android/src/com/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.FlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
