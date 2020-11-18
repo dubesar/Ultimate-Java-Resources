2e3bad15db7ba1a9d8597663481ea912bc1fa4eb
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-11-18 08:55:23 +0530

    Update FriendsPairing.java

--- Dynamic Programming/FriendsPairing.java
+++ Dynamic Programming/FriendsPairing.java
@@ -1,3 +1,7 @@
+/*Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. 
+Find out the total number of ways in which friends can remain single or can be paired up.
+*/
+
 import java.util.*;
 public class FriendsPairing {  
     static int countFriendsPairing(int n) 
