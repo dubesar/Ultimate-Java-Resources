1ff4d459c06f9aade67f9edf5c4feac8db3f1018
Neamul Islam Fahim <71332807+NeamulIslamFahim@users.noreply.github.com>
2022-10-29 13:59:43 +0600

    Updated done

--- JavaHackerrank/Java1DArray.java
+++ JavaHackerrank/Java1DArray.java
@@ -6,10 +6,10 @@ public class Solution {
 	   
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-int a[] = new int[n];
-for(int i = 0; i<a.length; i++) {
-    a[i] = scan.nextInt();
-}
+	int a[] = new int[n];
+	for(int i = 0; i<a.length; i++) {
+    		a[i] = scan.nextInt();
+	}
         scan.close();
 
         // Prints each sequential element in array a
