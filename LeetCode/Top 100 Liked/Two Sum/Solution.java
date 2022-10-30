1ff5bff7f7452434fa1fb040200361968a8ac8fe
Sourabh Varshney <sourabh.varshney@hotstar.com>
2022-10-30 14:07:51 +0530

    Added Two Sum Leetcode solution

---
+++ LeetCode/Top 100 Liked/Two Sum/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        Map<Integer,Integer> map=new HashMap<>();
+        int[] result=new int[2];
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-nums[i])){
+                result[1]=i;
+                result[0]=map.get(target-nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+}
